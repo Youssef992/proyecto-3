59dea7c6a8acf4993442bd20fffa44ba4f172fb8
Manuel Youssef Morales Matta <mmorales@dell1425.tbn.com.mx>
2022-06-03 12:49:00 -0500

    Se agregó Footer y Header, imports y exports

---
+++ amiibos.js
@@ -0,0 +1,29 @@
+export const getAmiibo = async () => {
+    const url = 'https://www.amiiboapi.com/api/amiibo/?';
+    const response = await fetch(url);
+    console.log(response);
+    const data = await response.json();
+    console.log(data);
+    var amiiboContainer = document.getElementById('container');
+    amiiboContainer.innerHTML = "";
+    var cadena = "";
+    var i = 0;
+    data.amiibo.forEach((amiibo, index) => {
+        if (i <= 50) {
+            cadena += `
+            <div class="col-3">
+                <div class="card">
+                    <img src="${amiibo.image}" alt="${amiibo.character}" class="card-img-top" id="amiibo-img">
+                    <div class="card-body">
+                        <h5 class="card-title">${amiibo.name}</h5>
+                        <p class="card-text"><b>Game Series:</b> ${amiibo.gameSeries}</p>
+                    </div>
+                </div>
+            </div>
+            `
+            i++;
+        }
+    });
+    amiiboContainer.innerHTML = `<div class="row">` + cadena + `</div>`;
+} 
+
---
+++ buscaamiibos.js
@@ -0,0 +1,30 @@
+const buscaAmiibo = async () => {
+    var textoAmiibo = document.getElementById('inputSearch').value;
+    const url = `https://www.amiiboapi.com/api/amiibo/?character=${textoAmiibo}`;
+    const response = await fetch(url);
+    console.log(response);
+    const data = await response.json();
+    console.log(data);
+    var amiiboContainer = document.getElementById('container');
+    amiiboContainer.innerHTML = "";
+    var cadena = "";
+    var i = 0;
+    data.amiibo.forEach((amiibo, index) => {
+        if (i <= 50) {
+            cadena += `
+            <div class="col-6">
+                <div class="card">
+                    <img src="${amiibo.image}" alt="${amiibo.character}" class="card-img-top" id="amiibo-img">
+                    <div class="card-body">
+                        <h5 class="card-title">${amiibo.character}</h5>
+                        <p class="card-text"><b>Game Series:</b> ${amiibo.gameSeries}</p>
+                    </div>
+                </div>
+            </div>
+            `
+            i++;
+        }
+    });
+    amiiboContainer.innerHTML = `<div class="row">` + cadena + `</div>`;
+
+} 
